9ef9cdeb21230c55e491735c01ceba49e11f6e14
jenemybill-lgtm <jenemybill@gmail.com>
2026-08-21 04:04:39 +0300

    Increase JSON and URL-encoded body size limits

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- DATABASE MODELS ---
 const userSchema = new mongoose.Schema({
